644c89a4b8d0a35abc22a430c8308ea7b1c7678e
chime1996 <chimechiagoziem@gmail.com>
2025-11-08 13:28:09 +0100

    commit via changes

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Assigning variables in dom
+
+const billValue = document.getElementById("bill");
+
+const fivePercent = document.getElementById("five");
+
+const tenPercent = document.getElementById("ten");
+
+const fifteenPercent = document.getElementById("fifteen");
+
+const twentyFivePercent = document.getElementById("twenty-five");
+
+const fiftyPercent = document.getElementById("fifty");
