210a2e6fbcaea43256386d468c316af819fa2081
chime1996 <chimechiagoziem@gmail.com>
2025-11-09 10:22:24 +0100

    commit via changes

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
 const $ = (id) => document.getElementById(id);
 
-const billValue = $("bill");
+let billValue = $("bill");
 
 const fivePercent = $("five");
 
@@ -23,3 +23,146 @@ const numberOfPeople = $("num-ppl");
 const tipAmount = $("tip-amt");
 
 const totalAmount = $("total-amt");
+
+const reset = document.querySelector(".btn-reset");
+
+fivePercent.addEventListener("click", function () {
+  const bill = billValue.value;
+
+  // calculate tip
+  let tipFive = Number((5 / 100) * bill);
+  let fiveTotal = tipFive + Number(bill); // ask question here
+
+  /*
+  Question 2
+  how do i remove number from infinity or how to curtail excess numbers like 1.3333333333333 thats to round off.
+
+
+  3. How to make sure there is no empty input eg, if number is empty, it will display enter number of people.
+
+  4. why did you reset bill and number of people to zero.
+
+  5. how to make custom percent accept enter key
+  */
+
+  //tip per person
+  const people = Number(numberOfPeople.value);
+
+  const totaltips = tipFive / people;
+
+  tipAmount.textContent = totaltips.toFixed(2);
+
+  // total per person
+  const totalPerson = bill / people + tipFive;
+  totalAmount.textContent = totalPerson.toFixed(2);
+});
+
+tenPercent.addEventListener("click", function () {
+  const bill = billValue.value;
+
+  // calculate tip
+  let tipTen = Number((10 / 100) * bill);
+  let fifteenTotal = tipTen + Number(bill);
+
+  //tip per person
+  const people = Number(numberOfPeople.value);
+
+  const totaltips = tipTen / people;
+
+  tipAmount.textContent = totaltips.toFixed(2);
+
+  // total per person
+  const totalPerson = bill / people + tipTen;
+  totalAmount.textContent = totalPerson.toFixed(2);
+});
+
+fifteenPercent.addEventListener("click", function () {
+  const bill = billValue.value;
+
+  // calculate tip
+  let tipFifteen = Number((15 / 100) * bill);
+  let fifteenTotal = tipFifteen + Number(bill);
+
+  //tip per person
+  const people = Number(numberOfPeople.value);
+
+  const totaltips = tipFifteen / people;
+
+  tipAmount.textContent = totaltips.toFixed(2);
+
+  // total per person
+  const totalPerson = bill / people + tipFifteen;
+  totalAmount.textContent = totalPerson.toFixed(2);
+});
+
+twentyFivePercent.addEventListener("click", function () {
+  const bill = billValue.value;
+
+  // calculate tip
+  let tipTwentyFive = Number((25 / 100) * bill);
+  let twentyFiveTotal = tipTwentyFive + Number(bill);
+
+  //tip per person
+  const people = Number(numberOfPeople.value);
+
+  const totaltips = tipTwentyFive / people;
+
+  tipAmount.textContent = totaltips.toFixed(2);
+
+  // total per person
+  const totalPerson = bill / people + tipTwentyFive;
+  totalAmount.textContent = totalPerson.toFixed(2);
+});
+
+fiftyPercent.addEventListener("click", function () {
+  const bill = billValue.value;
+
+  // calculate tip
+  let tipFifty = Number((50 / 100) * bill);
+  let fiftytotal = tipFifty + Number(bill);
+
+  //tip per person
+  const people = Number(numberOfPeople.value);
+
+  const totaltips = tipFifty / people;
+
+  tipAmount.textContent = totaltips.toFixed(2);
+
+  // total per person
+  const totalPerson = bill / people + tipFifty;
+  totalAmount.textContent = totalPerson.toFixed(2);
+});
+
+customTip.addEventListener("keydown", function (event) {
+  const bill = billValue.value;
+
+  // enable enter key to function
+  if (event.key === "Enter") {
+    const custom = Number(customTip.value);
+
+    // calculate tip
+    let tipCustom = Number((custom / 100) * bill);
+
+    let customTotal = tipCustom + Number(bill);
+
+    //tip per person
+    const people = Number(numberOfPeople.value);
+
+    const totaltips = tipCustom / people;
+
+    tipAmount.textContent = totaltips.toFixed(2);
+
+    // total per person
+    const totalPerson = bill / people + tipCustom;
+    totalAmount.textContent = totalPerson.toFixed(2);
+  }
+});
+
+// reset
+reset.addEventListener("click", function () {
+  billValue.value = "";
+  numberOfPeople.value = "";
+  tipAmount.textContent = "$0.00";
+  totalAmount.textContent = "$0.00";
+  customTip.value = "";
+});
